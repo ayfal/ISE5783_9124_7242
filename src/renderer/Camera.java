4907af004698da52ef10f12a761c2ab0d423edaf
ayfal <65089103+ayfal@users.noreply.github.com>
2023-05-08 17:46:14 +0300

    camera class implementation

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -3,11 +3,142 @@
  */
 package renderer;
 
-/**
- * @author Ariels
- *
- */
-public class Camera {
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
-	
+/**
+ * class Camera represents a camera in a 3D Cartesian coordinate system 
+ * 
+ * @author Ariels
+ */ 
+
+public class Camera {
+    private Point p0;
+    private Vector vUp;
+    private Vector vTo;
+    private Vector vRight;
+	double width;
+    double height;
+    double distance;
+    
+    // constructors
+    
+    /**
+     * constructor for camera
+     * 
+     * @param p0       camera position
+     * @param vTo      to vector
+     * @param vUp      up vector
+     */
+    public Camera(Point p0, Vector vTo, Vector vUp) {
+        this.p0 = p0;
+        this.vUp = vUp.normalize();
+        this.vTo = vTo.normalize();
+        if (vUp.dotProduct(vTo) != 0) {
+            throw new IllegalArgumentException("vUp and vTo are not orthogonal");
+        }
+        this.vRight = (vTo.crossProduct(vUp)).normalize();
+    }
+
+    // getters
+    /**
+     * get camera position
+     * 
+     * @return camera position
+     */
+    public Point getP0() {
+        return p0;
+    }
+
+    /**
+     * get up vector
+     * 
+     * @return up vector
+     */
+    public Vector getvUp() {
+        return vUp;
+    }
+
+    /**
+     * get to vector
+     * 
+     * @return to vector
+     */
+    public Vector getvTo() {
+        return vTo;
+    }
+
+    /**
+     * get right vector
+     * 
+     * @return right vector
+     */
+    public Vector getvRight() {
+        return vRight;
+    }
+
+    /**
+     * get width
+     * 
+     * @return width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * get height
+     * 
+     * @return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * get distance
+     * 
+     * @return distance
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+    //setters
+
+    /**
+     * set camera position
+     * @param width
+     * @param height
+     * @return camera
+     */
+    public Camera setVPSize(double width, double height){
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    /**
+     * set distance
+     * @param distance
+     * @return camera
+     */
+    public Camera setVPDistance(double distance){
+        this.distance = distance;
+        return this;
+    }
+    
+    /**
+     * construct ray through pixel
+     * @param nX number of columns
+     * @param nY number of rows
+     * @param j column index
+     * @param i row index
+     * @return ray
+     */
+    public Ray constructRay(int nX, int nY, int j, int i){
+        return null;
+    }
+
 }
