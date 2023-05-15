fdc3b7ee6a8bea7fe0631c424a1769fa44ac5c26
arieldavid777 <arieldav1408@gmail.com>
2023-05-15 17:47:12 +0300

    start of stage 5(color + image writer)

---
+++ src/unitests/renderer/ImageWriterTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package unitests.renderer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ariels
+ *
+ */
+class ImageWriterTest {
+
+	/**
+	 * Test method for {@link renderer.ImageWriter#ImageWriter(java.lang.String, int, int)}.
+	 */
+	@Test
+	void testImageWriter() {
+		
+	}
+
+	/**
+	 * Test method for {@link renderer.ImageWriter#writeToImage()}.
+	 */
+	@Test
+	void testWriteToImage() {
+	
+	}
+
+	/**
+	 * Test method for {@link renderer.ImageWriter#writePixel(int, int, primitives.Color)}.
+	 */
+	@Test
+	void testWritePixel() {
+	
+	}
+
+}
