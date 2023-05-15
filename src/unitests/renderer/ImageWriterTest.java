89b991c8849750df9dec367a881cb8807f2de154
arieldavid777 <arieldav1408@gmail.com>
2023-05-15 18:02:42 +0300

    write to image unitest

--- src/unitests/renderer/ImageWriterTest.java
+++ src/unitests/renderer/ImageWriterTest.java
@@ -17,11 +17,12 @@ import renderer.ImageWriter;
 class ImageWriterTest {
 
 	/**
-	 * Test method for {@link renderer.ImageWriter#ImageWriter(java.lang.String, int, int)}.
+	 * Test method for
+	 * {@link renderer.ImageWriter#ImageWriter(java.lang.String, int, int)}.
 	 */
 	@Test
 	void testImageWriter() {
-		
+
 	}
 
 	/**
@@ -29,34 +30,32 @@ class ImageWriterTest {
 	 */
 	@Test
 	void testWriteToImage() {
-		int nX=800;
-		int nY=500;
+		int nX = 801;
+		int nY = 501;
 
 		Color yellow = new Color(255d, 255d, 0);
-        Color red = new Color(255, 0, 0);
+		Color red = new Color(255, 0, 0);
 
-		ImageWriter imageWriter = new ImageWriter("yellow red grid",nX,nY);
+		ImageWriter imageWriter = new ImageWriter("yellow red grid", nX, nY);
 		for (int i = 0; i < nX; i++) {
-            for (int j = 0; j < nY; j++) {
-                if (i % 50 == 0 || j % 50 == 0)
-                    imageWriter.writePixel(i, j, red);
-                else
-                    imageWriter.writePixel(i, j, yellow);
-            }
-        }
-        imageWriter.writeToImage();
+			for (int j = 0; j < nY; j++) {
+				if (i % 50 == 0 || j % 50 == 0)
+					imageWriter.writePixel(i, j, red);
+				else
+					imageWriter.writePixel(i, j, yellow);
+			}
+		}
+		imageWriter.writeToImage();
 
-
-
-		
 	}
 
 	/**
-	 * Test method for {@link renderer.ImageWriter#writePixel(int, int, primitives.Color)}.
+	 * Test method for
+	 * {@link renderer.ImageWriter#writePixel(int, int, primitives.Color)}.
 	 */
 	@Test
 	void testWritePixel() {
-	
+
 	}
 
 }
