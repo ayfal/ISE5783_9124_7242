3a5b2f0c83260bc7a4c507ba3ed81f103cad96a7
ayfal <65089103+ayfal@users.noreply.github.com>
2023-05-21 22:36:22 +0300

    ray tacer base and basic

--- src/renderer/RayTracerBase.java
+++ src/renderer/RayTracerBase.java
@@ -3,10 +3,32 @@
  */
 package renderer;
 
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
 /**
+ * abstract class for ray tracers
+ * 
  * @author Ariel
  *
  */
 public abstract class RayTracerBase {
+    protected Scene scene;
 
+    //constructor
+    /**
+     * constructs a ray tracer with a given scene
+     * @param s scene
+     */
+    public RayTracerBase(Scene s) {
+        scene = s;
+    }
+
+    /**
+     * trace a ray and return the color of the closest intersection
+     * @param ray ray to trace
+     * @return color of the closest intersection
+     */
+    public abstract Color traceRay(Ray ray);
 }
