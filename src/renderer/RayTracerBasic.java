f897897a5bc48c2d8a209537fd20491888248865
ayfal <65089103+ayfal@users.noreply.github.com>
2023-05-21 22:37:00 +0300

    ray tracer basic

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -3,10 +3,29 @@
  */
 package renderer;
 
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
 /**
+ * a class for basic ray tracing that extends RayTracerBase
  * @author Ariel
  *
  */
 public class RayTracerBasic extends RayTracerBase {
+    
+        //constructor
+        /**
+        * constructs a ray tracer with a given scene
+        * @param s scene
+        */
+        public RayTracerBasic(Scene s) {
+            super(s);
+        }
+
+        @Override
+        public Color traceRay(Ray ray){
+            return null;
+        }
 
 }
