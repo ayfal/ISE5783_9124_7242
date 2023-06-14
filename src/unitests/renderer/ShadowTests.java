06d53b3c83bc9923e6fecb82e1c15bdd029fc7fe
ayfal <65089103+ayfal@users.noreply.github.com>
2023-06-14 23:59:00 +0300

    stage 7, phase 1, 6-8

--- src/unitests/renderer/ShadowTests.java
+++ src/unitests/renderer/ShadowTests.java
@@ -48,7 +48,7 @@ public class ShadowTests {
 	@Test
 	public void sphereTriangleMove1() {
 		sphereTriangleHelper("shadowSphereTriangleMove2", //
-				new Triangle(new Point(???, ???, ???), new Point(???, ???, ???), new Point(???, ???, ???)), //
+				new Triangle(new Point(-62, -32, 0), new Point(-32, -62, 0), new Point(-60, -60, -4)), //
 				new Point(-100, -100, 200));
 	}
 
@@ -58,7 +58,7 @@ public class ShadowTests {
 	@Test
 	public void sphereTriangleMove2() {
 		sphereTriangleHelper("shadowSphereTriangleMove1", //
-		      new Triangle(new Point(???, ???, ???), new Point(???, ???, ???), new Point(???, ???, ???)), //
+		      new Triangle(new Point(-49, -19, 0), new Point(-19, -49, 0), new Point(-47, -47, -4)), //
 				new Point(-100, -100, 200));
 	}
 
@@ -67,7 +67,7 @@ public class ShadowTests {
    public void sphereTriangleSpot1() {
       sphereTriangleHelper("shadowSphereTriangleSpot1", //
                            new Triangle(new Point(-70, -40, 0), new Point(-40, -70, 0), new Point(-68, -68, -4)), //
-                           new Point(???, ???, ???));
+				               new Point(-88, -88, 120));
    }
 
    /** Sphere-Triangle shading - move spot even more close */
@@ -75,7 +75,7 @@ public class ShadowTests {
    public void sphereTriangleSpot2() {
       sphereTriangleHelper("shadowSphereTriangleSpot2", //
                            new Triangle(new Point(-70, -40, 0), new Point(-40, -70, 0), new Point(-68, -68, -4)), //
-                           new Point(???, ???, ???));
+				               new Point(-76, -76, 70));
    }
 
    /** Produce a picture of a two triangles lighted by a spot light with a Sphere
@@ -90,7 +90,7 @@ public class ShadowTests {
                               .setMaterial(new Material().setKs(0.8).setShininess(60)), //
                            new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150)) //
                               .setMaterial(new Material().setKs(0.8).setShininess(60)), //
-                           new Sphere(new Point(0, 0, -11), 30d) //
+                           new Sphere(30d, new Point(0, 0, -11)) //
                               .setEmission(new Color(BLUE)) //
                               .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
       );
